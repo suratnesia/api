dedeafa374e04c4870c6b30f7d5132c9beeacd06
Erwin <user@Erwin-MacBook-Pro-539.local>
2026-07-09 20:57:46 +0700

    feat: add config management layer

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import "os"
+
+type Config struct {
+	Port       string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func Load() *Config {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	return &Config{
+		Port:       port,
+		DBHost:     dbHost,
+		DBPort:     dbPort,
+		DBUser:     dbUser,
+		DBPassword: dbPassword,
+		DBName:     dbName,
+	}
+}
